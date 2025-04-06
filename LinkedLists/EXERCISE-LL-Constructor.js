512ff256e905a16dc4b6428c5ec1f100434fbb6d
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2025-04-06 18:45:17 +0100

    Create LinkedLists with class constructor

--- LinkedLists/EXERCISE-LL-Constructor.js
+++ LinkedLists/EXERCISE-LL-Constructor.js
@@ -1,16 +1,22 @@
 // WRITE NODE CLASS HERE //
-//                       //
-//                       //
-//                       //
-//                       //
+
+class Node {
+    constructor(value){
+        this.value = value,
+        this.next = null
+    }
+
+}
 ///////////////////////////
 
 class LinkedList {
 	// WRITE LL CONSTRUCTOR HERE //
-	//                           //
-	//                           //
-	//                           //
-	//                           //
+	constructor(value){
+        const newNode = new Node(value)
+        this.head = newNode
+        this.tail = this.head // You can also set this.tail = newNode, because they are pointig to the same object(node)
+        this.length = 1
+    }
 	///////////////////////////////
 
     printList() {
@@ -20,29 +26,33 @@ class LinkedList {
             temp = temp.next;
         }
     }
-
-    getHead() {
-        if (this.head === null) {
-            console.log("Head: null");
-        } else {
-            console.log("Head: " + this.head.value);
+    getHead(){
+        if(this.head === null){
+            console.log('Head: null')
+        }else{
+            console.log('Head: ', this.head.value)
         }
     }
 
-    getTail() {
-        if (this.tail === null) {
-            console.log("Tail: null");
-        } else {
-            console.log("Tail: " + this.tail.value);
-        }
-    }
 
+   getTail(){
+    if(this.tail === null){
+        console.log('Tail: null')
+    }else{
+        console.log('Tail: ', this.tail.value)
+    }
+   }
     getLength() {
         console.log("Length: " + this.length);
     }
 
 }
 
+// let myLinkedList = new LinkedList(4);
+// console.log(myLinkedList.printList())
+// console.log(myLinkedList.getHead())
+
+// == Instead of consoling one by one we bundled all the methods in one function to test them all at once == //
 
 function test() {
     let myLinkedList = new LinkedList(4);
@@ -67,5 +77,13 @@ test();
     
     Linked List:
     4
+
+*/
+
+/*
+======= IMPORTANT =======
+- The 'new' keyword creates a new instance of the class. which means it creates a new object in memory. 
+- It calls the constructor method of the class, which initializes the properties of the object.
+
 
 */
