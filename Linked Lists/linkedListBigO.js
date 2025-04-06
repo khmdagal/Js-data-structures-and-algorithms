3d485608b250a60a842d8d0f63f7e181d9e36ba5
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2025-04-06 17:37:41 +0100

    Linked list of Big O and overview of performance of different operations

---
+++ Linked Lists/linkedListBigO.js
@@ -0,0 +1,17 @@
+/*
+Operation of Big O Notation                           Linked List              Arrays
+__________________________________________________________________________________________
+Unshifting (Adding Elements at the beginning)          O(1)                    O(n)
+Shifting (Removing Elements from the beginning)        O(1)                    O(n)
+Pushing (Adding Elements at the end)                   O(1)                    O(1)
+Poping (Removing Elements from the end)                O(n)                    O(1)
+Inserting (Adding Elements at a specific index)        O(n)                    O(n)
+Deleting (Removing Elements at a specific index)       O(n)                    O(n)
+Looking up By INDEX (Accessing Elements)               O(n)                    O(1)
+LOOKING UP BY VALUE (Searching Elements)               O(n)                    O(n)
+__________________________________________________________________________________________
+* The Big O notation is used to describe the performance or complexity of an algorithm.
+* It describes the worst-case scenario of an algorithm's performance.
+* It is used to compare the efficiency of different algorithms.
+* It is used to analyze the performance of an algorithm in terms of time and space complexity.
+*/
