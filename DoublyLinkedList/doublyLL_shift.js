c447b0f0fb1a106cd6ce09d1bec45a5251ebe902
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2025-06-12 21:36:49 +0100

    Add shift method

---
+++ DoublyLinkedList/doublyLL_shift.js
@@ -0,0 +1,96 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
+class DoublyLinkedList{
+    constructor(value){
+        const newNode = new Node(value)
+        this.head = newNode
+        this.tail = newNode
+        this.length = 1
+    }
+
+
+    push(value){
+        const newNode = new Node(value)
+        if(this.head === null){
+            this.head = newNode;
+            this.tail = newNode
+        }else{
+            this.tail.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+
+    pop() {
+        let temp = this.tail
+        if (this.length === 0) {
+            return undefined
+        } 
+        
+        if(this.length === 1){
+            this.head = null
+            this.tail = null
+        }else{
+            
+            this.tail = this.tail.prev
+            this.tail.next = null
+            temp.prev = null
+
+        }
+        this.length--
+        return temp
+    }
+
+    unshift(value){
+        const newNode = new Node(value)
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        } else{
+            newNode.next = this.head
+            this.head.prev = newNode
+            this.head = newNode
+        }
+
+        this.length++
+        return this
+    }
+
+    shift(){
+        if(!this.head) return undefined
+        let temp = this.head
+        if(this.length === 1){
+            this.head = null
+            this.tail = null
+        }else{
+            
+            this.head = this.head.next
+            this.head.prev = null
+            temp.next = null
+        }
+
+
+        this.length--
+        return temp
+    }
+}
+
+const myDoublyLinkedList = new DoublyLinkedList(0)
+myDoublyLinkedList.push(1)
+myDoublyLinkedList.push(2)
+
+console.log('====== Before shift method ====')
+console.log(myDoublyLinkedList)
+
+console.log('====== After shift method ====')
+myDoublyLinkedList.shift()
+// myDoublyLinkedList.shift()
+console.log(myDoublyLinkedList)
