8ce818ed09746d55553958fcb64453aeb04b255d
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2025-06-18 10:19:46 +0100

    Add queues and stacts constructor

---
+++ Stacks_and_queues/stackConstructor.js
@@ -0,0 +1,15 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+
+    }
+}
+
+class Stacks {
+    constructor(value){
+        const newNode = new Node(value);
+        this.top = newNode
+        this.hight = 1
+    }
+}
