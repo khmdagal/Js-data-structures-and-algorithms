f6ab5cd9fa9ad3b84877e7c5f6795ebdbea7706f
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2025-06-24 11:10:47 +0100

    push method for Stacks data structure

---
+++ Stacks_and_queues/pushMethod_Stacks.js
@@ -0,0 +1,37 @@
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+
+    }
+}
+
+class Stacks {
+    constructor(value){
+        const newNode = new Node(value);
+        this.top = newNode
+        this.hight = 1
+    }
+
+    push(value){
+        const newNode = new Node(value)
+        if(this.hight === 0){
+            this.top = newNode
+        } else{
+            newNode.next = this.top
+            this.top = newNode
+        }
+        this.hight++
+        return this
+        
+    }
+}
+
+const myStacks = new Stacks(4)
+myStacks.push(1)
+myStacks.push(2)
+myStacks.push(5)
+myStacks.push(8)
+
+
+console.log(myStacks)
