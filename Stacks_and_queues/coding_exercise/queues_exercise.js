e0503cc71b8536e1d0a07093b7e870939041b9bb
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2025-08-08 19:55:25 +0100

    Add enqueue method

--- Stacks_and_queues/coding_exercise/queues_exercise.js
+++ Stacks_and_queues/coding_exercise/queues_exercise.js
@@ -12,4 +12,25 @@ class queues {
         this.last = newNode
         this.length = 1
     }
+
+    enqueue(value){
+        const newNode = new Node(value)
+        if(this.length === 0){
+            this.first = newNode;
+            this.last = newNode;
+        }else{
+            this.last.next = newNode
+            this.last = newNode
+        }
+
+        this.length++
+        return this
+    }
 }
+
+const newQueue = new queues(1);
+
+newQueue.enqueue(2)
+newQueue.enqueue(3)
+newQueue.enqueue(4)
+console.log(newQueue)
