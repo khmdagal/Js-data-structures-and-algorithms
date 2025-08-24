aa059f85a2501c5bf2c9b7a1498e99647a52ec30
Khadar Dagal <81495872+khmdagal@users.noreply.github.com>
2025-08-24 16:40:20 +0100

    basic intro to hash tables

---
+++ hash_tables/basic_understanding.js
@@ -0,0 +1,41 @@
+//HASH TABLES
+/*
+Hash tables are data strucuture that efficiently stores and retrieve data
+into key-value pairs approach.
+
+JavaScript has build-in hash table with is objects {}
+
+
+// Thinks to keep in mind
+
+1. One way 
+
+2. Deterministic
+
+
+// Managing Collisions
+
+1) Separate Chaining
+Separate chaining is the precess of placing all the key-value pairs that are mapped in 
+a particular address in an one array or using linked-list.
+example1= arrays in array
+eg:- array that holds the address number [["key":"values"], ["key":"values"]]
+
+exmaple 2: Linked-list
+key1->key2->key3->key4
+
+
+2) Linear Probing
+(Open addressing)
+Linear Probing is the precess that places key-value pairs in an open address, and it kept looping 
+until it finds and open address to store the key-value data.
+
+// Js
+{
+"name": "Adam"
+}
+*/
+
+//console.log(hash("paul"))
+
+console.log(charCodeAt('nails'))
